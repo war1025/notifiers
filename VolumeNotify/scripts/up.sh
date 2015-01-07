cced6d06e4a06ba13b654da0ece57eb034f056e1
Wayne Rowcliffe <war1025@gmail.com>
2015-01-06 23:06:41 -0600

    Use pactl to control volume (Allows going above 100%)

--- VolumeNotify/scripts/up.sh
+++ VolumeNotify/scripts/up.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-amixer -D pulse sset Master 3%+ >> /dev/null
+SINK=$(pactl list short sinks | grep alsa | awk '{print $1}')
+
+pactl set-sink-volume $SINK +3% >> /dev/null
 
 ~/.local/share/notifiers/volume/show-volume.sh >> /dev/null
 
