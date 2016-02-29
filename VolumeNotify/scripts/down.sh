dfd7905f54b0cba1d8b68625d8d186f372900c7d
Wayne Rowcliffe <war1025@gmail.com>
2016-02-28 20:25:11 -0600

    Fix `pactl` call in `down.sh`

--- VolumeNotify/scripts/down.sh
+++ VolumeNotify/scripts/down.sh
@@ -2,6 +2,6 @@
 
 SINK=$(pactl list short sinks | grep alsa | awk '{print $1}')
 
-pactl set-sink-volume $SINK -- -3% >> /dev/null
+pactl set-sink-volume $SINK -3% >> /dev/null
 
 ~/.local/share/notifiers/volume/show-volume.sh >> /dev/null
